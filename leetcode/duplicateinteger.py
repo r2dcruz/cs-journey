b93112e9490e6552be92750caeda4cebc37bec69
Romaine De La Cruz <r2dcruz@gmail.com>
2024-09-02 21:44:23 -0700

    practice, twosum

---
+++ leetcode/duplicateinteger.py
@@ -0,0 +1,5 @@
+# given an integer array nums, return true if any value appears more than once in the array, otherwise return false
+
+class solution:
+    def __init__(self) -> None:
+        return
--- leetcode/twosum.py
+++ leetcode/twosum.py
@@ -41,3 +41,14 @@ class Solution:
             if diff in prevMap:
                 return [prevMap[diff],i]
             prevMap[n] = i        
+
+# given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j. return the smallest index first.
+
+class Solution:
+    def twosum(self, nums:List[int], target:int) -> List[int];
+        prevMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff],i]
+            prevMap[n] = i
